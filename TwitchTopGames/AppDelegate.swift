6bb6fbba45c6dfd02c6fa0a48e22082016e14b63
Aleksandr <croleguu@mail.ru>
2020-07-21 10:58:18 +0700

    initial setup

---
+++ TwitchTopGames/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TwitchTopGames
+//
+//  Created by Alexander on 21.07.2020.
+//  Copyright © 2020 Alexander Team. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ TwitchTopGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitchTopGames
+//
+//  Created by Alexander on 21.07.2020.
+//  Copyright © 2020 Alexander Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+}
+
